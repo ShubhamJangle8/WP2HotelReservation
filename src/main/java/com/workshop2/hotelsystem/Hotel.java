de93b367cd3fc39289cb4f3f126baf56513ffce1
jangle <shubhamjangale0000@gmail.com>
2020-10-15 15:31:46 +0530

    Added : Welcome message by Program

---
+++ src/main/java/com/workshop2/hotelsystem/Hotel.java
@@ -0,0 +1,32 @@
+package com.workshop2.hotelsystem;
+
+public class Hotel {
+	private String hotelName;
+	private int regularWeekDay;
+
+	public Hotel(String hotelName, int regularWeekDay) {
+		super();
+		this.hotelName = hotelName;
+		this.regularWeekDay = regularWeekDay;
+	}
+
+	public String getHotelName() {
+		return hotelName;
+	}
+
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+
+	public int getRegularWeekDayRate() {
+		return regularWeekDay;
+	}
+
+	public void setRegularWeekDayRate(int regularWeekDay) {
+		this.regularWeekDay = regularWeekDay;
+	}
+
+	public String toString() {
+		return "Hotel Name : " + hotelName + ", Normal Week day Rate : " + regularWeekDay;
+	}
+}
---
+++ src/main/java/com/workshop2/hotelsystem/HotelReservation.java
@@ -0,0 +1,16 @@
+package com.workshop2.hotelsystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotelReservation {
+	List<Hotel> hotelList;
+	
+	public HotelReservation() {
+		hotelList = new ArrayList<>();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Hotel Reservation System");
+	}
+}
